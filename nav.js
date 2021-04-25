5b4e5684d673c3ab99b8dc616b360872b193ec46
vishu sharma <vishu.sharma.cs.2019@miet.ac.in>
2021-04-25 16:22:32 +0530

    Flora and Fauna

---
+++ nav.js
@@ -0,0 +1,24 @@
+function navSlide() {
+    const mob = document.querySelector(".mob");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    
+    mob.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+        
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //Burger Animation
+        mob.classList.toggle("toggle");
+    });
+    
+}
+
+navSlide();
